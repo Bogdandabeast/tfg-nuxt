93aaa79efaa4002bced8108565958e2eb9e020eb
Bogdandabeast <bogdan.console@gmail.com>
2025-11-15 19:53:04 +0100

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  vite: {    plugins: [tailwindcss(),]},
 })
