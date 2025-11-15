2057ddf9dc61b2486cf1e46dd2fbe016ef8b40e7
Bogdandabeast <99488549+Bogdandabeast@users.noreply.github.com>
2025-11-15 21:27:25 +0000

    5 add and config testing library for test driven development (#8)
    * Implement code changes to enhance functionality and improve performance
    
    * feat: Add vitest configuration for unit and nuxt testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/test-utils/module"],
   vite: { plugins: [tailwindcss()] },
   css: ["./app/assets/css/main.css"],
   eslint: {
@@ -12,4 +12,6 @@ export default defineNuxtConfig({
       standalone: false,
     },
   },
-});
+
+})
+;
