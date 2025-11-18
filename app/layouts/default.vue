2cb4599378dce8df56c4b94d92441339ffc4f4d9
Bogdandabeast <bogdan.console@gmail.com>
2025-11-18 21:05:11 +0100

    feat: Update layout and add new pages with initial content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col gap-4 min-h-screen m-auto container">
     <LayoutHeader class="border-4 border-black" />
-    <main class="flex-1 border-4 border-black">
+    <main class="flex-1 flex border-4 border-black">
       <slot />
     </main>
     <LayoutFooter class="border-4 border-black" />
---
+++ app/pages/about.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex-1">
+    <h1>sdfdsf</h1>
+  </div>
+</template>
