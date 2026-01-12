b0f68632734cffdd03bc9c6966a501fb93cdb3c1
Bogdandabeast <bogdan.console@gmail.com>
2026-01-12 17:04:18 +0100

    fix: Add csrf token to every POST, PUT,PATCH AND DELETE request

--- app/stores/customers.ts
+++ app/stores/customers.ts
@@ -31,24 +31,30 @@ export const useCustomersStore = defineStore("customers", () => {
   async function createCustomer(customerData: Partial<Customer>) {
     if (!companiesStore.currentCompany?.id)
       return;
+    const { csrf } = useCsrf();
     await useFetch("/api/customers", {
       method: "POST",
       body: { ...customerData, company_id: companiesStore.currentCompany.id },
+      headers: { "csrf-token": csrf },
     });
     await refresh();
   }
 
   async function updateCustomer(id: number, customerData: Partial<Customer>) {
+    const { csrf } = useCsrf();
     await useFetch(`/api/customers/${id}`, {
       method: "PUT",
       body: customerData,
+      headers: { "csrf-token": csrf },
     });
     await refresh();
   }
 
   async function deleteCustomer(id: number) {
+    const { csrf } = useCsrf();
     await useFetch(`/api/customers/${id}`, {
       method: "DELETE",
+      headers: { "csrf-token": csrf },
     });
     await refresh();
   }
