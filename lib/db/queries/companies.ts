ecda3a90368d15e9d822b84158e66da3ca055efa
Bogdandabeast <bogdan.console@gmail.com>
2026-01-16 19:21:09 +0100

    feat: add missing query

--- lib/db/queries/companies.ts
+++ lib/db/queries/companies.ts
@@ -1,4 +1,10 @@
-import type { companiesTable } from "../schema/companies";
+import { eq } from "drizzle-orm";
+import { db } from "../index";
+import { companiesTable } from "../schema/companies";
 
 export type CompanyInsert = typeof companiesTable.$inferInsert;
 export type Company = typeof companiesTable.$inferSelect;
+
+export async function getCompanyById(id: number) {
+  return db.select().from(companiesTable).where(eq(companiesTable.id, id));
+}
