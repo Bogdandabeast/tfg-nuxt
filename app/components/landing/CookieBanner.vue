d661ba34cbcc2239e46d88d399c93e4ed995e869
Bogdandabeast <bogdan.console@gmail.com>
2026-02-03 20:42:07 +0100

    fix: update email sender and add cookiebanner

---
+++ app/components/landing/CookieBanner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UModal :dismissible="false" title="Modal non-dismissible">
+    <template #body>
+      <Placeholder class="h-48" />
+    </template>
+  </UModal>
+</template>
--- utils/emailsender.ts
+++ utils/emailsender.ts
@@ -1,10 +1,6 @@
 import type { SendEmailData } from "~~/types/api";
 import { z } from "zod";
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("Missing RESEND_API_KEY environment variable");
-}
-
 const sendEmailSchema = z.object({
   from: z.string().email(),
   to: z.string().email(),
@@ -17,7 +13,7 @@ const sendEmailSchema = z.object({
 export async function sendEmail(data: SendEmailData) {
   try {
     const validatedData = sendEmailSchema.parse(data);
-    const result = await $fetch(String(process.env.RESEND_API), {
+    const result = await $fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: {
         "Authorization": `Bearer ${process.env.RESEND_API_KEY}`,
